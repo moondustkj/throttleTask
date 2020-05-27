e27ec4d830488269bb8fcd2d3b62851c678a21ae
komalJituri88 <komaljituri88@gmail.com>
2020-05-27 18:45:20 +0530

    Updates

---
+++ precache-manifest.d71413004a5943eebb837fcf8d8558d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57123243a05c99af5711cfebda823342",
+    "url": "/throttleTask/index.html"
+  },
+  {
+    "revision": "1bf43941f774c012d622",
+    "url": "/throttleTask/static/css/2.9cbbadcf.chunk.css"
+  },
+  {
+    "revision": "ef3fcb566e09820baaf0",
+    "url": "/throttleTask/static/css/main.b3be6795.chunk.css"
+  },
+  {
+    "revision": "1bf43941f774c012d622",
+    "url": "/throttleTask/static/js/2.5602be6a.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/throttleTask/static/js/2.5602be6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3fcb566e09820baaf0",
+    "url": "/throttleTask/static/js/main.fad4f6d6.chunk.js"
+  },
+  {
+    "revision": "2bafc0e4718051a9684f",
+    "url": "/throttleTask/static/js/runtime-main.36a73ab2.js"
+  }
+]);
